fd4f1415202d99d8ae021339f9674592e3f73462
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-08-13 00:41:11 +0300

    cleaned the code, added searchTerm variable and deleted the fetch from script Relates #12

--- public/HomePage/script.js
+++ public/HomePage/script.js
@@ -1,34 +1,16 @@
 const searchBox = document.getElementById('search');
 const matchList = document.getElementById('match-values');
-// ? fetch data from specific source
-const fetch = (url, cb) => {
-  console.log(`fetch url: ${url}`);
-  if (document.readyState === 'complete') {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          const data = JSON.parse(xhr.responseText);
-          console.log(`fetched data\n${data}`);
-          cb(data);
-        } else {
-          console.error(xhr.responseText);
-        }
-      }
-    };
-    xhr.open('GET', url, true);
-    xhr.send();
-  }
-};
 
+// Cleaning chlidren of a section and get it empty
 const clearingSection = (section) => {
   while (section.firstElementChild) {
     section.firstChild.remove();
   }
 };
 const handlingDOMvaluesMenu = (matchedValues, listSection) => {
-// clear menu
+  // clear menu
   clearingSection(listSection);
+  // displaying values in html
   matchedValues.slice(0, 10).forEach((element) => {
     const valueDiv = document.createElement('div');
     valueDiv.className = 'value';
@@ -39,11 +21,10 @@ const handlingDOMvaluesMenu = (matchedValues, listSection) => {
 };
 // ? get the books that contains the value of the input from books.json file
 const getMatchedValues = (arrayOfBooks) => {
-  console.log('in getMatchedValues func and here is the arrrrraaayyyy of bboks\n', arrayOfBooks);
-  if (searchBox.value) {
+  const searchTerm = searchBox.value;
+  if (searchTerm) {
     // eslint-disable-next-line max-len
-    const matchedValues = arrayOfBooks.filter((el) => el.toLowerCase().includes((searchBox.value).toLowerCase()));
-    console.log('filtereddddddd aray\n', matchedValues);
+    const matchedValues = arrayOfBooks.filter((el) => el.toLowerCase().includes((searchTerm).toLowerCase()));
     handlingDOMvaluesMenu(matchedValues, matchList);
   } else {
     // delete menu
