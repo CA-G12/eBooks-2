7d52f86eca51e81d5e16bc928e9a85abde9ceef6
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-08-12 13:12:56 +0300

    Handled home page Relates #10

---
+++ src/handlers/homeHandler.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime-types');
+
+const homeHandler = (req, res) => {
+  const extension = path.extname(req);
+  const filePath = path.join(__dirname, '..', '..', req);
+  console.log(filePath);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.log(`error in accessing homepage${extension}${err}`);
+      res.writeHead(500, { 'Content-Type': 'text/html' });
+      res.end('<h1>Server Error</h1>');
+    } else {
+      res.writeHead(200, { 'Content-Type': mime.lookup(extension) });
+      res.end(data);
+    }
+  });
+};
+module.exports = homeHandler;
