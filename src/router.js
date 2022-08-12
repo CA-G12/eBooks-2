f37df33c7dab73b42a93276bdcc22e21907b72b3
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-08-12 13:14:33 +0300

    Added suitable function for each endpoint Relates #10

--- src/router.js
+++ src/router.js
@@ -1,13 +1,24 @@
+const homeHandler = require('./handlers/homeHandler');
+const landingHTMLHandler = require('./handlers/landingHTMLHandler');
+const landingHandler = require('./handlers/landingHandler');
+const booksHandler = require('./handlers/booksHandler');
+
 const router = (req, res) => {
   const endpoint = req.url;
   console.log(endpoint);
 
   if (endpoint === '/') {
-    //   landingHandler
-  } else if (endpoint.includes('public')) {
-    console.log('in public handler');
-    //   homeHandler
+    landingHTMLHandler(res);
+  } else if (endpoint.includes('LandingPage')) {
+    console.log('in Landing handler');
+    landingHandler(endpoint, res);
+  } else if (endpoint.includes('HomePage')) {
+    console.log('in home handler');
+    homeHandler(endpoint, res);
   } else if (endpoint === '/books') {
+    console.log('in books handler');
+    booksHandler(endpoint, res);
+
     //   booksHandler
   } else if (endpoint === '/search') {
     //   SearchHandler
