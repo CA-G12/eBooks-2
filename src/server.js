6d5b67a68134b9c00264743be91658bc850e28e3
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-08-13 16:07:13 +0300

    edit port related to #25

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = process.env.HOSTNAME || 'localhost';
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 const router = require('./router');
 
 const server = http.createServer(router);
